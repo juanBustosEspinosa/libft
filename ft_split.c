c7a092e727389b6dcafd132d53e7333b5f5e037c
juanBustosEspinosa <suertenani@gmail.com>
2025-10-01 18:27:48 +0200

    split terminado

--- ft_split.c
+++ ft_split.c
@@ -6,30 +6,14 @@
 /*   By: jbustos- <jbustos-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 11:57:43 by jbustos-          #+#    #+#             */
-/*   Updated: 2025/09/22 11:56:22 by jbustos-         ###   ########.fr       */
+/*   Updated: 2025/10/01 18:25:52 by jbustos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <string.h>
+#include <unistd.h>
 
-/*static size_t	prueba(char *dst, const char *src, size_t size)
-{
-	size_t	count;
-	size_t	max;
-
-	max = strlen(src);
-	count = 0;
-	if (size == 0)
-		return (max);
-	while (size - 1 > count && src[count] != '\0')
-	{
-		dst[count] = src[count];
-		count++;
-	}
-	dst[count] = '\0';
-	return (max);
-}*/
 static int count_delimiter(char const *s, char c)
 {
 	int t;
@@ -38,17 +22,25 @@ static int count_delimiter(char const *s, char c)
 	t = 0;
 	count = 0;
 
+	if(!s || s[0] == '\0')
+		return (0);
+
+	while (s[count] == c)
+		count++;
 	while (s[count] != '\0')
 	{
-		while (s[count] == c)
-			count++;
-		if (s[count] != '\0')
+		if (s[count] == c)
+		{
 			t++;
-		
-		while (s[count] != '\0' && s[count] != c)
+			while (s[count] == c)
+				count++;
+		}
+		else
 			count++;
 	}
-	return (t);
+	if (s[count - 1] == c)
+		t--;
+	return (t + 1);
 }
 
 static int count_letter(char const *s, char c, int *count)
@@ -78,41 +70,24 @@ char	**ft_split(char const *s, char c)
 	int	delimiter;
 	int i;
 	int letter;
-	
-	if (!s || c == '\0')
-    	return (NULL);
+		
 	i = 0;
 	count = 0;
 	delimiter = count_delimiter(s, c);
 	ptr = (char **)malloc((delimiter + 1) * sizeof(char *));
 	if (!ptr)
-			return (NULL);
-	ptr[delimiter] = NULL;
-
+		return (NULL);
 	while (delimiter > i)
 	{
+		if (s[0] == '\0')
+			break ;
 		letter = count_letter(s, c, &count);
-		if (letter > 0)
-		{
 		ptr[i] = (char *)malloc(letter + 1);
 		if (!ptr[i])
 			return (NULL);
-		ft_strlcpy(ptr[i],s + (count-letter),letter + 1);//strlcpy
+		ft_strlcpy(ptr[i],s + (count-letter),letter + 1);
 		i++;
-		}
 	}
+	ptr[delimiter] = NULL;
 	return (ptr);
-}
-
-int main(int arc, char **avg)
-{
-	(void)arc;
-	char **c = ft_split((char const *)avg[1], '\0');
-	int i = 0;
-	while (c[i] != NULL)
-	{
-		printf("%s\n", c[i]);
-		i++;
-	}
-	
 }
